139aa105dab0836c701adff33d6187cdcf5afbac
Abhinandan <abhimehta1324@gmail.com>
2025-06-06 02:40:08 +0200

    3d map walk around added

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ function loadGoogleMapsScript() {
   script.async = true;
   document.head.appendChild(script);
 }
-
 function initMap() {
-  const berlin = { lat: 52.5200, lng: 13.4050 };
+const startLocation = { lat: 52.521328, lng: 13.409760 };
+
   const map = new google.maps.Map(document.getElementById("map"), {
+    center: startLocation,
     zoom: 15,
-    center: berlin,
   });
 
-  new google.maps.Marker({
-    position: berlin,
-    map: map,
-  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("map"), {
+      position: startLocation,
+      pov: {
+        heading: 270,
+        pitch: 0
+      },
+      zoom: 1
+    });
+
+  map.setStreetView(panorama);
 }
 
-// Kick things off
 loadGoogleMapsScript();
